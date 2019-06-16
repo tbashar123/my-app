7add2259e5cacd99e77d96158749467d2aecc6a1
tbashar123 <tbashar09@gmail.com>
2019-06-15 20:17:40 -0400

    Update NavigationBar.js

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { Nav, Navbar } from "react-bootstrap";
 import styled from "styled-components";
 
-const Styles = styled.div`
+/*const Styles = styled.div`
   .navbar {
     background-color: #222;
   }
@@ -15,32 +15,36 @@ const Styles = styled.div`
       color: white;
     }
   }
-`;
+`;*/
 
 export const NavigationBar = () => (
-  <Styles>
-    <Navbar expand="lg">
-      <Navbar.Brand href="/">TJ Campus</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
-            <Nav.Item>
-                <Nav.Link>
+ <Styles>
+  <div>
+//     <Navbar expand="lg">
+//       <Navbar.Brand href="/">TJ Campus</Navbar.Brand>
+//       <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//       <Navbar.Collapse id="basic-navbar-nav">
+//         <Nav className="ml-auto">
+//             <Nav.Item>
+//                 <Nav.Link>
+           <ul id="nav">
                     <Link to="/">Home</Link>
-                </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link>
+//                 </Nav.Link>
+//             </Nav.Item>
+//             <Nav.Item>
+//                 <Nav.Link>
                     <Link to="/campus">Campus</Link>
-                </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link>
+//                 </Nav.Link>
+//             </Nav.Item>
+//             <Nav.Item>
+//                 <Nav.Link>
                     <Link to="/student">Student</Link>
-                </Nav.Link>
-            </Nav.Item>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+  </ul>
+//                 </Nav.Link>
+//             </Nav.Item>
+//         </Nav>
+//       </Navbar.Collapse>
+//     </Navbar>
+  </div>
   </Styles>
 )
